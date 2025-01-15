a48b8a3a9fca33d87bbccca80e3b5c1d47055740
NISHCHAY DEEPAK MITTAL <u24ai044@aid.svnit.ac.in>
2025-01-15 22:04:48 +0530

    Delete Lab1 directory

--- Lab1/Asg0_prog6.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Enter any number you want to reverse "))
-reverse = 0
-remainder = 1
-print("The original number was",number)
-while number != 0:
-    remainder = number%10
-    reverse = reverse*10 + remainder
-    number = number//10
-print("The reversed number is",reverse)
