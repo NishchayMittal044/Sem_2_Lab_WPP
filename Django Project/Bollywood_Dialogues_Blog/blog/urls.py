91918c5fd0675e8f22e11efb5afa7d3115414717
NISHCHAY DEEPAK MITTAL <u24ai044@aid.svnit.ac.in>
2025-04-24 03:17:22 +0530

    Delete Django Project/Bollywood_Dialogues_Blog directory

--- Django Project/Bollywood_Dialogues_Blog/blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.post_list, name='post_list'),# Home page with all posts
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),# Individual post page
-]
